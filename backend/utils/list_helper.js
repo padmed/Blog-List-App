ba6e1785bce5c79045b4098f8e8f76becfc1b503
padmed <ddarchinian@gmail.com>
2023-07-10 15:10:06 +0400

    Write favoriteBlog method and appropriate tests

--- backend/utils/list_helper.js
+++ backend/utils/list_helper.js
@@ -6,4 +6,22 @@ const totalLikes = (array) => {
   return array.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
-module.exports = { dummy, totalLikes };
+const favoriteBlog = (array) => {
+  if (array.length > 0) {
+    const topBlog = array.reduce((acc, obj) => {
+      if (obj.likes > acc.likes) {
+        return obj;
+      }
+      return acc;
+    });
+
+    return {
+      title: topBlog.title,
+      author: topBlog.author,
+      likes: topBlog.likes,
+    };
+  }
+  return null;
+};
+
+module.exports = { dummy, totalLikes, favoriteBlog };
