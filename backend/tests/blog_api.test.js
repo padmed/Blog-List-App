98732236cb9150497c14cfe879c84ddda3c0e1a3
padmed <ddarchinian@gmail.com>
2023-07-15 23:05:01 +0400

    Write test for Delete request handler

--- backend/tests/blog_api.test.js
+++ backend/tests/blog_api.test.js
@@ -1,7 +1,7 @@
 const app = require("../app");
 const supertest = require("supertest");
 const Blog = require("../models/blog");
-const { initialBlogs, blogsInDb } = require("./list_api_helper");
+const { initialBlogs, blogsInDb, nonExistingId } = require("./list_api_helper");
 
 const api = supertest(app);
 
@@ -13,71 +13,94 @@ beforeEach(async () => {
   await Promise.all(promisesArray);
 });
 
-test("/api/blogs GET method works", async () => {
-  const request = await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
+describe("Initial tests", () => {
+  test("/api/blogs GET method works", async () => {
+    const request = await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
 
-  expect(request.body).toHaveLength(initialBlogs.length);
-});
+    expect(request.body).toHaveLength(initialBlogs.length);
+  });
 
-test("id property is defined instead of _id", async () => {
-  const blogs = await blogsInDb();
-  blogs.forEach((blog) => {
-    expect(blog).toHaveProperty("id");
-    expect(blog).not.toHaveProperty("_id");
+  test("Id property is defined instead of _id", async () => {
+    const blogs = await blogsInDb();
+    blogs.forEach((blog) => {
+      expect(blog).toHaveProperty("id");
+      expect(blog).not.toHaveProperty("_id");
+    });
   });
 });
 
-test("blog is saved in the database", async () => {
-  const newBlog = {
-    title: "test",
-    author: "Bob",
-    url: "bob.com",
-    likes: 0,
-  };
+describe("POST tests", () => {
+  test("Blog is saved in the database", async () => {
+    const newBlog = {
+      title: "test",
+      author: "Bob",
+      url: "bob.com",
+      likes: 0,
+    };
 
-  const reqResult = await api.post("/api/blogs").send(newBlog).expect(201);
-  const blogsAtEnd = await blogsInDb();
+    const reqResult = await api.post("/api/blogs").send(newBlog).expect(201);
+    const blogsAtEnd = await blogsInDb();
 
-  expect(blogsAtEnd).toHaveLength(initialBlogs.length + 1);
-  expect(blogsAtEnd).toContainEqual(reqResult.body);
+    expect(blogsAtEnd).toHaveLength(initialBlogs.length + 1);
+    expect(blogsAtEnd).toContainEqual(reqResult.body);
+  });
+
+  test("Adding a blog without likes, sets value of likes to 0", async () => {
+    const newBlog = {
+      title: "test",
+      author: "Bob",
+      url: "bob.com",
+    };
+
+    const reqResult = await api.post("/api/blogs").send(newBlog).expect(201);
+    expect(reqResult.body).toHaveProperty("likes", 0);
+  });
+
+  test("Missing URL field returns 400", async () => {
+    const newBlog = {
+      title: "test",
+      author: "Bob",
+      likes: 0,
+    };
+
+    await api.post("/api/blogs").send(newBlog).expect(400);
+    const blogsAtEnd = await blogsInDb();
+
+    expect(blogsAtEnd).toHaveLength(initialBlogs.length);
+  });
+
+  test("Missing Title field returns 400", async () => {
+    const newBlog = {
+      author: "Bob",
+      url: "bob.com",
+      likes: 0,
+    };
+
+    await api.post("/api/blogs").send(newBlog).expect(400);
+    const blogsAtEnd = await blogsInDb();
+
+    expect(blogsAtEnd).toHaveLength(initialBlogs.length);
+  });
 });
 
-test("likes default value is 0", async () => {
-  const newBlog = {
-    title: "test",
-    author: "Bob",
-    url: "bob.com",
-  };
+describe("DELETE tests", () => {
+  test("DELETE method works", async () => {
+    const blogs = await blogsInDb();
+    const blogToDelete = blogs[0];
 
-  const reqResult = await api.post("/api/blogs").send(newBlog).expect(201);
-  expect(reqResult.body).toHaveProperty("likes", 0);
-});
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
 
-test("missing URL field returns 400", async () => {
-  const newBlog = {
-    title: "test",
-    author: "Bob",
-    likes: 0,
-  };
+    const notesAtEnd = await blogsInDb();
 
-  await api.post("/api/blogs").send(newBlog).expect(400);
-  const blogsAtEnd = await blogsInDb();
+    expect(notesAtEnd).toHaveLength(initialBlogs.length - 1);
+    expect(notesAtEnd).not.toContainEqual(blogToDelete);
+  });
 
-  expect(blogsAtEnd).toHaveLength(initialBlogs.length);
-});
-
-test("missing Title field returns 400", async () => {
-  const newBlog = {
-    author: "Bob",
-    url: "bob.com",
-    likes: 0,
-  };
-
-  await api.post("/api/blogs").send(newBlog).expect(400);
-  const blogsAtEnd = await blogsInDb();
-
-  expect(blogsAtEnd).toHaveLength(initialBlogs.length);
+  test("Deleting non-existing note returns 204 as well", async () => {
+    const id = await nonExistingId();
+    await api.delete(`/api/blogs/${id}`).expect(204);
+  });
 });
--- backend/tests/list_api_helper.js
+++ backend/tests/list_api_helper.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require("mongoose");
 const Blog = require("../models/blog");
 
 const initialBlogs = [
@@ -20,7 +21,22 @@ const blogsInDb = async () => {
   return blogs.map((blog) => blog.toJSON());
 };
 
+const nonExistingId = async () => {
+  const blog = new Blog({
+    title: "dudu's blog",
+    author: "Bob",
+    url: "bob.com",
+    likes: 1232,
+  });
+  const blogSaved = await blog.save();
+  const id = blogSaved.id;
+  await Blog.findByIdAndRemove(id);
+
+  return id;
+};
+
 module.exports = {
   initialBlogs,
   blogsInDb,
+  nonExistingId,
 };
