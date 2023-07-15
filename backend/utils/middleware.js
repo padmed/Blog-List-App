4e2d9e1f00b61caaa39655403009601464792f67
padmed <ddarchinian@gmail.com>
2023-07-15 19:13:12 +0400

    Add required fields in Blog scheme

--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -7,7 +7,7 @@ const errorHandler = (error, request, response, next) => {
   logger.error("--------------");
 
   if (error.name === "ValidationError") {
-    return response.status(422).json({
+    return response.status(400).json({
       error: error.name,
       errorMessage: error.message,
     });
