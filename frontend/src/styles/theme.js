589407a814e79240801283c1b8299e32474c1fe3
padmed <ddarchinian@gmail.com>
2023-10-16 15:46:59 +0400

    Add colors

--- frontend/src/styles/theme.js
+++ frontend/src/styles/theme.js
@@ -4,7 +4,9 @@ export const colors = {
   beige: "rgb(248,245,241)",
   black: "rgb(0,0,0)",
   orange: "rgb(249, 123, 34)",
-  red: "#f44336",
+  orangeLight: "rgba(249, 123, 34, 0.9)",
+  red: "rgb(244, 67, 54)",
+  redLight: "rgba(244, 67, 54, 0.7)",
   green: "#388e3c",
 };
 
