cbfc8053ba3f45df82ff20aec9eec7a48761266b
padmed <ddarchinian@gmail.com>
2023-11-11 16:56:40 +0400

    Style comments

--- frontend/src/components/Comments.js
+++ frontend/src/components/Comments.js
@@ -3,6 +3,19 @@ import PropTypes from "prop-types";
 import { addComment } from "../reducers/blogReducer";
 import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
+import TextField from "@mui/material/TextField";
+import { Button, Divider, List, ListItem, Paper } from "@mui/material";
+import {
+  commentInputStyle,
+  submitButtonStyle,
+  commentStyle,
+} from "../styles/styles";
+import {
+  commentInputLayout,
+  commentButtonLayout,
+  commentFormLayout,
+  commentLayout,
+} from "../styles/layoutStyles";
 
 const Comments = ({ commentsOfBlog }) => {
   const dispatch = useDispatch();
@@ -11,22 +24,44 @@ const Comments = ({ commentsOfBlog }) => {
   const handleAddComment = (e) => {
     e.preventDefault();
     const comment = e.target.comment.value;
-    e.target.comment.value = "";
-    dispatch(addComment(id, comment));
+
+    if (comment.length > 1) {
+      e.target.comment.value = "";
+      dispatch(addComment(id, comment));
+    }
   };
 
   return (
     <div>
-      <h4>Comments</h4>
-      <form onSubmit={handleAddComment}>
-        <input name="comment"></input>
-        <button type="submit">Add Comment</button>
+      <form onSubmit={handleAddComment} style={{ ...commentFormLayout }}>
+        <TextField
+          name="comment"
+          variant="standard"
+          placeholder="Add comment"
+          multiline
+          sx={{ ...commentInputStyle, ...commentInputLayout }}
+        />
+        <Button
+          type="submit"
+          variant="contained"
+          sx={{ ...submitButtonStyle, ...commentButtonLayout }}
+        >
+          Submit
+        </Button>
       </form>
-      <ul>
+      {commentsOfBlog.length > 0 && <h4>Comments</h4>}
+      <List>
         {commentsOfBlog.map((comment) => (
-          <li key={uuidv4()}>{comment}</li>
+          <Paper
+            elevation={2}
+            key={uuidv4()}
+            style={{ ...commentStyle, ...commentLayout }}
+          >
+            <ListItem>{comment}</ListItem>
+            <Divider />
+          </Paper>
         ))}
-      </ul>
+      </List>
     </div>
   );
 };
