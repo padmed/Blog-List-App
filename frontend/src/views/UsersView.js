ced36b37955113afcd6312a51a28de1e76af9399
padmed <ddarchinian@gmail.com>
2023-11-21 18:37:45 +0400

    Refactor component

--- frontend/src/views/UsersView.js
+++ frontend/src/views/UsersView.js
@@ -1,32 +1,7 @@
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import Users from "../components/Users";
 
 const UsersView = () => {
-  const users = useSelector((state) => state.allUsers);
-
-  return (
-    <div>
-      <h1>Users</h1>
-      <table>
-        <thead>
-          <tr>
-            <td>Names</td>
-            <td>Blogs Created</td>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td>
-                <Link to={`/users/${user.id}`}>{user.name}</Link>
-              </td>
-              <td>{user.blogs.length}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+  return <Users />;
 };
 
 export default UsersView;
